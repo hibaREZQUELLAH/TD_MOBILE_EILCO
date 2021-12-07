152d08f3bb2e3a4fd17a1fc4ecad3dfccdac5135
Hiba REZQUELLAH <hiba.rezquellah.elv@eilco-ulco.fr>
2021-12-07 14:12:57 +0100

    add tds

--- TD4_android/app/src/main/java/com/example/td4/NewsActivity.java
+++ TD4_android/app/src/main/java/com/example/td4/NewsActivity.java
@@ -45,6 +45,9 @@ public class NewsActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
